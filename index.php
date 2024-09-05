3dd1eb07831bad0496e91928ecdc8837ceaab9f1
sazidul rahman <100462697+sazidulcse@users.noreply.github.com>
2024-09-05 13:36:07 +0600

    new commit

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 
 
-echo "hello world ";
+
+$first_name = "sazidul";
+$last_name = "Rahman";
+var_dump($first_name,$last_name);
+echo "my first name is : ".$first_name. "and list name is : ".$last_name."\n";
+echo "my first name is : {$first_name} and last name is {$last_name} \n";
+printf("my first name is %s and my last name is %s \n",$first_name,$last_name);
